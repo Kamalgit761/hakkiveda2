d426bbbeb5569548a9d9fde5d1ed290ff3e40b07
Kamalgit761 <kdiljit761@gmail.com>
2026-06-30 14:06:04 +0530

    Set default backend URL for API

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL ||
+  "https://hakkiveda2-production.up.railway.app";
 export const API = `${BACKEND_URL}/api`;
 
 const api = axios.create({ baseURL: API });
