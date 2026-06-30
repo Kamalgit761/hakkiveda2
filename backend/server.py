2459170eed523db92fb892c24cff07c9e431b9f3
Kamalgit761 <kdiljit761@gmail.com>
2026-06-30 20:01:10 +0530

    fix: use os.getenv for environment variables

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ from google.genai import types as genai_types
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.getenv('MONGO_URL')
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
